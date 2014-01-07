cf6d660cff282838d09f3f5b246524c97cebffd9
0xC0000054 <0xC0000054@users.noreply.github.com>
2014-01-07 05:45:27 +0000

    File version updated to 1.0.0.7.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("null54")]
 [assembly: AssemblyProduct("WebPFileType")]
-[assembly: AssemblyCopyright("Copyright © 2011-2012 Nicholas Hayes (aka null54)")]
+[assembly: AssemblyCopyright("Copyright © 2011-2014 Nicholas Hayes (aka null54)")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.6")]
-[assembly: AssemblyFileVersion("1.0.0.6")]
+[assembly: AssemblyVersion("1.0.0.7")]
+[assembly: AssemblyFileVersion("1.0.0.7")]
