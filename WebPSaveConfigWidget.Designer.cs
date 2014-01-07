2f6d66fcbbe827222f8eb72d354f5fa31f0ccb29
0xC0000054 <0xC0000054@users.noreply.github.com>
2014-01-07 05:43:38 +0000

    Added support for controlling the Spatial Noise Shaping in lossy compressed images. Updated to disable the lossy compression settings when using lossless compression. The presets will now update the controls with the proper settings.

--- WebPSaveConfigWidget.Designer.cs
+++ WebPSaveConfigWidget.Designer.cs
@@ -42,11 +42,14 @@ namespace WebPFileType
             this.fileSizeTxt = new System.Windows.Forms.TextBox();
             this.donateLink = new System.Windows.Forms.LinkLabel();
             this.keepMetaDataCb = new System.Windows.Forms.CheckBox();
-            this.fileSizeLbl = new PaintDotNet.HeaderLabel();
+            this.noiseShapingUpDown = new System.Windows.Forms.NumericUpDown();
+            this.noiseShapingSlider = new System.Windows.Forms.TrackBar();
+            this.strengthLbl = new PaintDotNet.HeaderLabel();
+            this.noiseShapingLbl = new PaintDotNet.HeaderLabel();
             this.encodeMethodLbl = new PaintDotNet.HeaderLabel();
+            this.fileSizeLbl = new PaintDotNet.HeaderLabel();
             this.filterTypeLbl = new PaintDotNet.HeaderLabel();
             this.sharpnessLbl = new PaintDotNet.HeaderLabel();
-            this.strengthLbl = new PaintDotNet.HeaderLabel();
             this.qualityLbl = new PaintDotNet.HeaderLabel();
             this.presetLbl = new PaintDotNet.HeaderLabel();
             ((System.ComponentModel.ISupportInitialize)(this.qualitySlider)).BeginInit();
@@ -57,6 +60,8 @@ namespace WebPFileType
             ((System.ComponentModel.ISupportInitialize)(this.sharpnessSlider)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.encodeMethodUpDown)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.encodeMethodSlider)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.noiseShapingUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.noiseShapingSlider)).BeginInit();
             this.SuspendLayout();
             // 
             // presetCbo
@@ -78,7 +83,7 @@ namespace WebPFileType
             // 
             // qualitySlider
             // 
-            this.qualitySlider.Location = new System.Drawing.Point(7, 69);
+            this.qualitySlider.Location = new System.Drawing.Point(0, 69);
             this.qualitySlider.Maximum = 100;
             this.qualitySlider.Name = "qualitySlider";
             this.qualitySlider.Size = new System.Drawing.Size(104, 45);
@@ -88,7 +93,7 @@ namespace WebPFileType
             // 
             // qualityUpDown
             // 
-            this.qualityUpDown.Location = new System.Drawing.Point(107, 69);
+            this.qualityUpDown.Location = new System.Drawing.Point(103, 69);
             this.qualityUpDown.Name = "qualityUpDown";
             this.qualityUpDown.Size = new System.Drawing.Size(43, 20);
             this.qualityUpDown.TabIndex = 4;
@@ -96,15 +101,15 @@ namespace WebPFileType
             // 
             // strengthUpDown
             // 
-            this.strengthUpDown.Location = new System.Drawing.Point(104, 120);
+            this.strengthUpDown.Location = new System.Drawing.Point(103, 227);
             this.strengthUpDown.Name = "strengthUpDown";
             this.strengthUpDown.Size = new System.Drawing.Size(43, 20);
             this.strengthUpDown.TabIndex = 7;
-            this.strengthUpDown.ValueChanged += new System.EventHandler(this.strengthUpDown1_ValueChanged);
+            this.strengthUpDown.ValueChanged += new System.EventHandler(this.strengthUpDown_ValueChanged);
             // 
             // strengthSlider
             // 
-            this.strengthSlider.Location = new System.Drawing.Point(4, 120);
+            this.strengthSlider.Location = new System.Drawing.Point(0, 226);
             this.strengthSlider.Maximum = 100;
             this.strengthSlider.Name = "strengthSlider";
             this.strengthSlider.Size = new System.Drawing.Size(104, 45);
@@ -114,7 +119,7 @@ namespace WebPFileType
             // 
             // sharpnessUpDown
             // 
-            this.sharpnessUpDown.Location = new System.Drawing.Point(106, 172);
+            this.sharpnessUpDown.Location = new System.Drawing.Point(103, 279);
             this.sharpnessUpDown.Maximum = new decimal(new int[] {
             7,
             0,
@@ -127,7 +132,7 @@ namespace WebPFileType
             // 
             // sharpnessSlider
             // 
-            this.sharpnessSlider.Location = new System.Drawing.Point(6, 172);
+            this.sharpnessSlider.Location = new System.Drawing.Point(0, 279);
             this.sharpnessSlider.Maximum = 7;
             this.sharpnessSlider.Name = "sharpnessSlider";
             this.sharpnessSlider.Size = new System.Drawing.Size(104, 45);
@@ -141,7 +146,7 @@ namespace WebPFileType
             this.filterTypeCbo.Items.AddRange(new object[] {
             "Simple",
             "Strong"});
-            this.filterTypeCbo.Location = new System.Drawing.Point(4, 223);
+            this.filterTypeCbo.Location = new System.Drawing.Point(3, 330);
             this.filterTypeCbo.Name = "filterTypeCbo";
             this.filterTypeCbo.Size = new System.Drawing.Size(124, 21);
             this.filterTypeCbo.TabIndex = 15;
@@ -149,7 +154,7 @@ namespace WebPFileType
             // 
             // encodeMethodUpDown
             // 
-            this.encodeMethodUpDown.Location = new System.Drawing.Point(103, 268);
+            this.encodeMethodUpDown.Location = new System.Drawing.Point(103, 124);
             this.encodeMethodUpDown.Maximum = new decimal(new int[] {
             6,
             0,
@@ -157,21 +162,21 @@ namespace WebPFileType
             0});
             this.encodeMethodUpDown.Name = "encodeMethodUpDown";
             this.encodeMethodUpDown.Size = new System.Drawing.Size(43, 20);
-            this.encodeMethodUpDown.TabIndex = 18;
+            this.encodeMethodUpDown.TabIndex = 6;
             this.encodeMethodUpDown.ValueChanged += new System.EventHandler(this.encodeMethodUpDown_ValueChanged);
             // 
             // encodeMethodSlider
             // 
-            this.encodeMethodSlider.Location = new System.Drawing.Point(3, 268);
+            this.encodeMethodSlider.Location = new System.Drawing.Point(0, 125);
             this.encodeMethodSlider.Maximum = 6;
             this.encodeMethodSlider.Name = "encodeMethodSlider";
             this.encodeMethodSlider.Size = new System.Drawing.Size(104, 45);
-            this.encodeMethodSlider.TabIndex = 17;
+            this.encodeMethodSlider.TabIndex = 5;
             this.encodeMethodSlider.ValueChanged += new System.EventHandler(this.encodeMethodSlider_ValueChanged);
             // 
             // fileSizeTxt
             // 
-            this.fileSizeTxt.Location = new System.Drawing.Point(4, 319);
+            this.fileSizeTxt.Location = new System.Drawing.Point(2, 375);
             this.fileSizeTxt.Name = "fileSizeTxt";
             this.fileSizeTxt.Size = new System.Drawing.Size(100, 20);
             this.fileSizeTxt.TabIndex = 20;
@@ -181,7 +186,7 @@ namespace WebPFileType
             // donateLink
             // 
             this.donateLink.AutoSize = true;
-            this.donateLink.Location = new System.Drawing.Point(4, 365);
+            this.donateLink.Location = new System.Drawing.Point(4, 422);
             this.donateLink.Name = "donateLink";
             this.donateLink.Size = new System.Drawing.Size(45, 13);
             this.donateLink.TabIndex = 21;
@@ -192,7 +197,7 @@ namespace WebPFileType
             // keepMetaDataCb
             // 
             this.keepMetaDataCb.AutoSize = true;
-            this.keepMetaDataCb.Location = new System.Drawing.Point(7, 345);
+            this.keepMetaDataCb.Location = new System.Drawing.Point(7, 402);
             this.keepMetaDataCb.Name = "keepMetaDataCb";
             this.keepMetaDataCb.Size = new System.Drawing.Size(101, 17);
             this.keepMetaDataCb.TabIndex = 22;
@@ -200,30 +205,68 @@ namespace WebPFileType
             this.keepMetaDataCb.UseVisualStyleBackColor = true;
             this.keepMetaDataCb.CheckedChanged += new System.EventHandler(this.keepMetaDataCb_CheckedChanged);
             // 
-            // fileSizeLbl
+            // noiseShapingUpDown
             // 
-            this.fileSizeLbl.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.fileSizeLbl.Location = new System.Drawing.Point(7, 298);
-            this.fileSizeLbl.Name = "fileSizeLbl";
-            this.fileSizeLbl.Size = new System.Drawing.Size(144, 14);
-            this.fileSizeLbl.TabIndex = 19;
-            this.fileSizeLbl.TabStop = false;
-            this.fileSizeLbl.Text = "File size in bytes";
+            this.noiseShapingUpDown.Location = new System.Drawing.Point(103, 176);
+            this.noiseShapingUpDown.Name = "noiseShapingUpDown";
+            this.noiseShapingUpDown.Size = new System.Drawing.Size(43, 20);
+            this.noiseShapingUpDown.TabIndex = 8;
+            this.noiseShapingUpDown.ValueChanged += new System.EventHandler(this.noiseShapingUpDown_ValueChanged);
+            // 
+            // noiseShapingSlider
+            // 
+            this.noiseShapingSlider.Location = new System.Drawing.Point(0, 176);
+            this.noiseShapingSlider.Maximum = 100;
+            this.noiseShapingSlider.Name = "noiseShapingSlider";
+            this.noiseShapingSlider.Size = new System.Drawing.Size(104, 45);
+            this.noiseShapingSlider.TabIndex = 7;
+            this.noiseShapingSlider.TickFrequency = 10;
+            this.noiseShapingSlider.ValueChanged += new System.EventHandler(this.noiseShapingSlider_ValueChanged);
+            // 
+            // strengthLbl
+            // 
+            this.strengthLbl.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.strengthLbl.Location = new System.Drawing.Point(0, 206);
+            this.strengthLbl.Name = "strengthLbl";
+            this.strengthLbl.Size = new System.Drawing.Size(144, 14);
+            this.strengthLbl.TabIndex = 5;
+            this.strengthLbl.TabStop = false;
+            this.strengthLbl.Text = "Filter Strength";
+            // 
+            // noiseShapingLbl
+            // 
+            this.noiseShapingLbl.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.noiseShapingLbl.Location = new System.Drawing.Point(0, 155);
+            this.noiseShapingLbl.Name = "noiseShapingLbl";
+            this.noiseShapingLbl.Size = new System.Drawing.Size(144, 14);
+            this.noiseShapingLbl.TabIndex = 23;
+            this.noiseShapingLbl.TabStop = false;
+            this.noiseShapingLbl.Text = "Spatial Noise Shaping";
             // 
             // encodeMethodLbl
             // 
             this.encodeMethodLbl.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.encodeMethodLbl.Location = new System.Drawing.Point(1, 248);
+            this.encodeMethodLbl.Location = new System.Drawing.Point(5, 104);
             this.encodeMethodLbl.Name = "encodeMethodLbl";
             this.encodeMethodLbl.Size = new System.Drawing.Size(144, 14);
             this.encodeMethodLbl.TabIndex = 16;
             this.encodeMethodLbl.TabStop = false;
             this.encodeMethodLbl.Text = "Encoding Speed / Quality";
             // 
+            // fileSizeLbl
+            // 
+            this.fileSizeLbl.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.fileSizeLbl.Location = new System.Drawing.Point(5, 354);
+            this.fileSizeLbl.Name = "fileSizeLbl";
+            this.fileSizeLbl.Size = new System.Drawing.Size(144, 14);
+            this.fileSizeLbl.TabIndex = 19;
+            this.fileSizeLbl.TabStop = false;
+            this.fileSizeLbl.Text = "File size in bytes";
+            // 
             // filterTypeLbl
             // 
             this.filterTypeLbl.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.filterTypeLbl.Location = new System.Drawing.Point(3, 203);
+            this.filterTypeLbl.Location = new System.Drawing.Point(2, 310);
             this.filterTypeLbl.Name = "filterTypeLbl";
             this.filterTypeLbl.Size = new System.Drawing.Size(144, 14);
             this.filterTypeLbl.TabIndex = 14;
@@ -233,22 +276,12 @@ namespace WebPFileType
             // sharpnessLbl
             // 
             this.sharpnessLbl.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.sharpnessLbl.Location = new System.Drawing.Point(4, 152);
+            this.sharpnessLbl.Location = new System.Drawing.Point(3, 259);
             this.sharpnessLbl.Name = "sharpnessLbl";
             this.sharpnessLbl.Size = new System.Drawing.Size(144, 14);
             this.sharpnessLbl.TabIndex = 11;
             this.sharpnessLbl.TabStop = false;
             this.sharpnessLbl.Text = "Sharpness";
-            // 
-            // strengthLbl
-            // 
-            this.strengthLbl.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.strengthLbl.Location = new System.Drawing.Point(1, 99);
-            this.strengthLbl.Name = "strengthLbl";
-            this.strengthLbl.Size = new System.Drawing.Size(144, 14);
-            this.strengthLbl.TabIndex = 5;
-            this.strengthLbl.TabStop = false;
-            this.strengthLbl.Text = "Filter Strength";
             // 
             // qualityLbl
             // 
@@ -274,13 +307,15 @@ namespace WebPFileType
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.strengthLbl);
+            this.Controls.Add(this.noiseShapingUpDown);
+            this.Controls.Add(this.noiseShapingSlider);
+            this.Controls.Add(this.noiseShapingLbl);
+            this.Controls.Add(this.encodeMethodLbl);
             this.Controls.Add(this.keepMetaDataCb);
             this.Controls.Add(this.donateLink);
             this.Controls.Add(this.fileSizeTxt);
             this.Controls.Add(this.fileSizeLbl);
-            this.Controls.Add(this.encodeMethodUpDown);
-            this.Controls.Add(this.encodeMethodSlider);
-            this.Controls.Add(this.encodeMethodLbl);
             this.Controls.Add(this.filterTypeCbo);
             this.Controls.Add(this.filterTypeLbl);
             this.Controls.Add(this.sharpnessUpDown);
@@ -288,14 +323,15 @@ namespace WebPFileType
             this.Controls.Add(this.sharpnessLbl);
             this.Controls.Add(this.strengthUpDown);
             this.Controls.Add(this.strengthSlider);
-            this.Controls.Add(this.strengthLbl);
             this.Controls.Add(this.qualityUpDown);
             this.Controls.Add(this.qualitySlider);
             this.Controls.Add(this.qualityLbl);
             this.Controls.Add(this.presetCbo);
             this.Controls.Add(this.presetLbl);
+            this.Controls.Add(this.encodeMethodUpDown);
+            this.Controls.Add(this.encodeMethodSlider);
             this.Name = "WebPSaveConfigWidget";
-            this.Size = new System.Drawing.Size(150, 390);
+            this.Size = new System.Drawing.Size(150, 442);
             ((System.ComponentModel.ISupportInitialize)(this.qualitySlider)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.qualityUpDown)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.strengthUpDown)).EndInit();
@@ -304,6 +340,8 @@ namespace WebPFileType
             ((System.ComponentModel.ISupportInitialize)(this.sharpnessSlider)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.encodeMethodUpDown)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.encodeMethodSlider)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.noiseShapingUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.noiseShapingSlider)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -331,5 +369,8 @@ namespace WebPFileType
         private System.Windows.Forms.TextBox fileSizeTxt;
         private System.Windows.Forms.LinkLabel donateLink;
         private System.Windows.Forms.CheckBox keepMetaDataCb;
+        private System.Windows.Forms.NumericUpDown noiseShapingUpDown;
+        private System.Windows.Forms.TrackBar noiseShapingSlider;
+        private HeaderLabel noiseShapingLbl;
     }
 }
--- WebPSaveConfigWidget.cs
+++ WebPSaveConfigWidget.cs
@@ -12,6 +12,8 @@ namespace WebPFileType
 {
     public partial class WebPSaveConfigWidget : SaveConfigWidget
     {
+        private int suspendTokenUpdateCounter;
+
         public WebPSaveConfigWidget()
         {
             InitializeComponent();
@@ -24,47 +26,106 @@ namespace WebPFileType
 
         protected override void InitTokenFromWidget()
         {
-            ((WebPSaveConfigToken)this.token).Preset = (WebPPreset)presetCbo.SelectedIndex;
-            ((WebPSaveConfigToken)this.token).Quality = qualitySlider.Value;
-            ((WebPSaveConfigToken)this.token).FilterStrength = strengthSlider.Value;
-            ((WebPSaveConfigToken)this.token).Sharpness = sharpnessSlider.Value;
-            ((WebPSaveConfigToken)this.token).FilterType = (WebPFilterType)filterTypeCbo.SelectedIndex;
-            ((WebPSaveConfigToken)this.token).Method = encodeMethodSlider.Value;
-            ((WebPSaveConfigToken)this.token).FileSize = (!fileSizeTxt.Enabled || string.IsNullOrEmpty(fileSizeTxt.Text)) ? 0 : int.Parse(fileSizeTxt.Text);
-            ((WebPSaveConfigToken)this.token).EncodeMetaData = keepMetaDataCb.Checked;
+            WebPSaveConfigToken configToken = (WebPSaveConfigToken)base.token; 
+            
+            configToken.Preset = (WebPPreset)presetCbo.SelectedIndex;
+            configToken.Quality = qualitySlider.Value;
+            configToken.Method = encodeMethodSlider.Value;
+            configToken.NoiseShaping = noiseShapingSlider.Value;
+            configToken.FilterStrength = strengthSlider.Value;
+            configToken.Sharpness = sharpnessSlider.Value;
+            configToken.FilterType = (WebPFilterType)filterTypeCbo.SelectedIndex;
+            configToken.FileSize = (!fileSizeTxt.Enabled || string.IsNullOrEmpty(fileSizeTxt.Text)) ? 0 : int.Parse(fileSizeTxt.Text);
+            configToken.EncodeMetaData = keepMetaDataCb.Checked;
         }
 
         protected override void InitWidgetFromToken(PaintDotNet.SaveConfigToken sourceToken)
         {
-            if (sourceToken is WebPSaveConfigToken)
+            WebPSaveConfigToken configToken = (WebPSaveConfigToken)sourceToken;
+
+            // Disable the UpdateToken function, this fixes a race condition with the controls that are updated by the UpdatePresetSliders method.
+            this.suspendTokenUpdateCounter++;
+
+            this.presetCbo.SelectedIndex = (int)configToken.Preset;
+            this.qualitySlider.Value = configToken.Quality;
+            this.encodeMethodSlider.Value = configToken.Method;
+            this.noiseShapingSlider.Value = configToken.NoiseShaping;
+            this.strengthSlider.Value = configToken.FilterStrength;
+            this.sharpnessSlider.Value = configToken.Sharpness;
+            this.filterTypeCbo.SelectedIndex = (int)configToken.FilterType;
+            this.fileSizeTxt.Text = configToken.FileSize > 0 ? configToken.FileSize.ToString() : string.Empty;
+            this.keepMetaDataCb.Checked = configToken.EncodeMetaData;
+
+            this.suspendTokenUpdateCounter--;
+        }
+
+        private void UpdateConfigToken()
+        {
+            if (suspendTokenUpdateCounter == 0)
             {
-                WebPSaveConfigToken configToken = (WebPSaveConfigToken)sourceToken;
-                this.presetCbo.SelectedIndex = (int)configToken.Preset;
-                this.qualitySlider.Value = configToken.Quality;
-                this.strengthSlider.Value = configToken.FilterStrength;
-                this.sharpnessSlider.Value = configToken.Sharpness;
-                this.filterTypeCbo.SelectedIndex = (int)configToken.FilterType;
-                this.encodeMethodSlider.Value = configToken.Method;
-                this.fileSizeTxt.Text = configToken.FileSize > 0 ? configToken.FileSize.ToString() : string.Empty;
-                this.keepMetaDataCb.Checked = configToken.EncodeMetaData;
-            }
-            else
-            {
-                this.presetCbo.SelectedIndex = (int)WebPPreset.Photo;
-                this.qualitySlider.Value = 95;
-                this.strengthSlider.Value = 20;
-                this.sharpnessSlider.Value = 0;
-                this.filterTypeCbo.SelectedIndex = (int)WebPFilterType.Simple;
-                this.encodeMethodSlider.Value = 4;
-                this.fileSizeTxt.Text = string.Empty;
-                this.keepMetaDataCb.Checked = true;
+                UpdateToken();
             }
         }
 
+        private void UpdatePresetSliders(WebPPreset preset)
+        {
+            this.suspendTokenUpdateCounter++;
+
+            switch (preset)
+            {
+                case WebPPreset.Picture:
+                    this.noiseShapingSlider.Value = 80;
+                    this.sharpnessSlider.Value = 4;
+                    this.strengthSlider.Value = 35;
+                    break;
+                case WebPPreset.Photo:
+                    this.noiseShapingSlider.Value = 80;
+                    this.sharpnessSlider.Value = 3;
+                    this.strengthSlider.Value = 30;
+                    break;
+                case WebPPreset.Drawing:
+                    this.noiseShapingSlider.Value = 80;
+                    this.sharpnessSlider.Value = 4;
+                    this.strengthSlider.Value = 35;
+                    break;
+                case WebPPreset.Icon:
+                    this.noiseShapingSlider.Value = 25;
+                    this.sharpnessSlider.Value = 6;
+                    this.strengthSlider.Value = 10;
+                    break;
+                case WebPPreset.Text:
+                    this.noiseShapingSlider.Value = 0;
+                    this.sharpnessSlider.Value = 0;
+                    this.strengthSlider.Value = 0;
+                    break;
+                case WebPPreset.Default:
+                default:
+                    this.noiseShapingSlider.Value = 50;
+                    this.strengthSlider.Value = 60;
+                    this.sharpnessSlider.Value = 0;
+                    break;
+            }
+
+            this.suspendTokenUpdateCounter--;
+        }
+
+        private void EnableLossyEncodingOptions(bool enabled)
+        {
+            this.noiseShapingSlider.Enabled = enabled;
+            this.noiseShapingUpDown.Enabled = enabled;
+            this.strengthSlider.Enabled = enabled;
+            this.strengthUpDown.Enabled = enabled;
+            this.sharpnessSlider.Enabled = enabled;
+            this.sharpnessUpDown.Enabled = enabled;
+            this.filterTypeCbo.Enabled = enabled;
+            this.fileSizeTxt.Enabled = enabled;
+        }
 
         private void presetCbo_SelectedIndexChanged(object sender, EventArgs e)
         {
-            this.UpdateToken();
+            UpdatePresetSliders((WebPPreset)this.presetCbo.SelectedIndex);
+
+            this.UpdateConfigToken();
         }
 
         private void qualitySlider_ValueChanged(object sender, EventArgs e)
@@ -74,9 +135,9 @@ namespace WebPFileType
                 this.qualityUpDown.Value = this.qualitySlider.Value;
             }
 
-            fileSizeTxt.Enabled = this.qualitySlider.Value < 100;
+            EnableLossyEncodingOptions(this.qualitySlider.Value < 100);
 
-            this.UpdateToken();
+            this.UpdateConfigToken();
         }
 
         private void qualityUpDown_ValueChanged(object sender, EventArgs e)
@@ -87,6 +148,42 @@ namespace WebPFileType
             }
         }
 
+        private void encodeMethodSlider_ValueChanged(object sender, EventArgs e)
+        {
+            if (encodeMethodUpDown.Value != encodeMethodSlider.Value)
+            {
+                this.encodeMethodUpDown.Value = this.encodeMethodSlider.Value;
+            }
+
+            this.UpdateConfigToken();
+        }
+
+        private void encodeMethodUpDown_ValueChanged(object sender, EventArgs e)
+        {
+            if (encodeMethodSlider.Value != (int)encodeMethodUpDown.Value)
+            {
+                this.encodeMethodSlider.Value = (int)encodeMethodUpDown.Value;
+            }
+        }
+
+        private void noiseShapingSlider_ValueChanged(object sender, EventArgs e)
+        {
+            if (noiseShapingUpDown.Value != noiseShapingSlider.Value)
+            {
+                this.noiseShapingUpDown.Value = this.noiseShapingSlider.Value;
+            }
+
+            this.UpdateConfigToken();
+        }
+
+        private void noiseShapingUpDown_ValueChanged(object sender, EventArgs e)
+        {
+            if (noiseShapingSlider.Value != noiseShapingUpDown.Value)
+            {
+                this.noiseShapingSlider.Value = (int)this.noiseShapingUpDown.Value;
+            }
+        }
+
         private void strengthSlider_ValueChanged(object sender, EventArgs e)
         {
             if (strengthUpDown.Value != strengthSlider.Value)
@@ -94,10 +191,10 @@ namespace WebPFileType
                 this.strengthUpDown.Value = this.strengthSlider.Value;
             }
 
-            this.UpdateToken();
+            this.UpdateConfigToken();
         }
 
-        private void strengthUpDown1_ValueChanged(object sender, EventArgs e)
+        private void strengthUpDown_ValueChanged(object sender, EventArgs e)
         {
             if (strengthSlider.Value != (int)strengthUpDown.Value)
             {
@@ -112,7 +209,7 @@ namespace WebPFileType
                 this.sharpnessUpDown.Value = this.sharpnessSlider.Value;
             }
 
-            this.UpdateToken();
+            this.UpdateConfigToken();
         }
 
         private void sharpnessUpDown_ValueChanged(object sender, EventArgs e)
@@ -125,25 +222,7 @@ namespace WebPFileType
 
         private void filterTypeCbo_SelectedIndexChanged(object sender, EventArgs e)
         {
-            this.UpdateToken();
-        }
-
-        private void encodeMethodSlider_ValueChanged(object sender, EventArgs e)
-        {
-            if (encodeMethodUpDown.Value != encodeMethodSlider.Value)
-            {
-                this.encodeMethodUpDown.Value = this.encodeMethodSlider.Value;
-            }
-
-            this.UpdateToken();
-        }
-
-        private void encodeMethodUpDown_ValueChanged(object sender, EventArgs e)
-        {
-            if (encodeMethodSlider.Value != (int)encodeMethodUpDown.Value)
-            {
-                this.encodeMethodSlider.Value = (int)encodeMethodUpDown.Value;
-            }
+            this.UpdateConfigToken();
         }
 
         private void fileSizeTxt_KeyDown(object sender, KeyEventArgs e)
@@ -163,12 +242,12 @@ namespace WebPFileType
 
         private void fileSizeTxt_TextChanged(object sender, EventArgs e)
         {
-            this.UpdateToken();
+            this.UpdateConfigToken();
         }
 
         private void keepMetaDataCb_CheckedChanged(object sender, EventArgs e)
         {
-            this.UpdateToken();
+            this.UpdateConfigToken();
         }
 
         private void donateLink_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
