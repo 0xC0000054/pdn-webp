278276d4792322d63a7c5eb4622c77cc42b0d544
0xC0000054 <0xC0000054@users.noreply.github.com>
2014-10-19 14:12:28 +0000

    Initial commit.

---
+++ PluginSupportInfo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using PaintDotNet;
+using System.Reflection;
+
+namespace WebPFileType
+{
+    class PluginSupportInfo : IPluginSupportInfo
+    {
+        public string Author
+        {
+            get 
+            {
+                return "null54";
+            }
+        }
+
+        public string Copyright
+        {
+            get 
+            {
+                return ((AssemblyCopyrightAttribute)(typeof(PluginSupportInfo).Assembly.GetCustomAttributes(typeof(AssemblyCopyrightAttribute), false)[0])).Copyright;
+            }
+        }
+
+        public string DisplayName
+        {
+            get 
+            {
+                return "WebP FileType";
+            }
+        }
+
+        public Version Version
+        {
+            get 
+            {
+                return typeof(PluginSupportInfo).Assembly.GetName().Version;
+            }
+        }
+
+        public Uri WebsiteUri
+        {
+            get
+            {
+                return new Uri("http://www.getpaint.net/redirect/plugins.html");
+            }
+        }
+    }
+}
