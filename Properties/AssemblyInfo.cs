df12b92c85a76c61024dec6c181b96e84b16a9b5
0xC0000054 <0xC0000054@users.noreply.github.com>
2017-02-04 05:22:19 +0000

    Updated the file version to 1.0.6.0 and the copyright year to 2017.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("null54")]
 [assembly: AssemblyProduct("WebPFileType")]
-[assembly: AssemblyCopyright("Copyright © 2011-2016 Nicholas Hayes (aka null54)")]
+[assembly: AssemblyCopyright("Copyright © 2011-2017 Nicholas Hayes (aka null54)")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.5.0")]
-[assembly: AssemblyFileVersion("1.0.5.0")]
+[assembly: AssemblyVersion("1.0.6.0")]
+[assembly: AssemblyFileVersion("1.0.6.0")]
