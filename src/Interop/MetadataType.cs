e4205c471e39a756193dbf0a345bd0ecb66f6a01
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2024-05-14 21:27:10 -0600

    Bump copyright year

--- src/Interop/MetadataType.cs
+++ src/Interop/MetadataType.cs
@@ -3,7 +3,7 @@
 // This file is part of pdn-webp, a FileType plugin for Paint.NET
 // that loads and saves WebP images.
 //
-// Copyright (c) 2011-2023 Nicholas Hayes
+// Copyright (c) 2011-2024 Nicholas Hayes
 //
 // This file is licensed under the MIT License.
 // See LICENSE.txt for complete licensing and attribution information.
