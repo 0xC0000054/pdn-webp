4cb7422bb752e5c38024819d3598353c9d98f185
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2026-01-09 20:04:19 -0700

    Update copyright year

--- src/Exif/MetadataHelpers.cs
+++ src/Exif/MetadataHelpers.cs
@@ -3,7 +3,7 @@
 // This file is part of pdn-webp, a FileType plugin for Paint.NET
 // that loads and saves WebP images.
 //
-// Copyright (c) 2011-2025 Nicholas Hayes
+// Copyright (c) 2011-2026 Nicholas Hayes
 //
 // This file is licensed under the MIT License.
 // See LICENSE.txt for complete licensing and attribution information.
